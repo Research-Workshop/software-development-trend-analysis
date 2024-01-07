9b9b9e42137646678692ddee86d2bdc71686cf78
Noman5237 <anonyman637@gmail.com>
2024-01-07 12:31:16 +0600

    added commits data for vscode

--- scripts/get-commits.sh
+++
@@ -1,1 +0,0 @@
-git log --pretty=format:"%h,%an,%ai,\"%s\"" master > commits.txt
---
+++ tools/bpt/extensions/sonarqube_analysis-generic.sh
@@ -0,0 +1,13 @@
+key=$1
+src=$2
+version=$3
+url=$4
+token=$5
+
+echo "Running Analysis on $src with $token sent to $url -> $key"
+sonar-scanner \
+  -Dsonar.projectKey=$key \
+  -Dsonar.projectVersion=$version \
+  -Dsonar.sources="$src" \
+  -Dsonar.host.url=$url \
+  -Dsonar.login=$token

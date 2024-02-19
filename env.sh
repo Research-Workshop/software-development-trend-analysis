275e54fc9e1972a63789b86ba04334c834b3f822
Noman5237 <anonyman637@gmail.com>
2024-02-19 12:44:17 +0600

    commit

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+export PROJECT_ROOT=/home/noman637/Projects/Research/software-metrics-systematic-study
+
+export PATH=$PATH:$PROJECT_ROOT/tools/bpt
+export PATH=$PATH:$PROJECT_ROOT/tools/sonar-scanner-5.0.1.3006-linux/bin
+export BPT_PATH=/home/noman637/Projects/Research/software-metrics-systematic-study/tools/bpt/bpt
+export MONGO_URL=mongodb://root:password@127.0.0.1:27017/?directConnection=true
+export SONARQUBE_TOKEN=squ_b30f63460de86ab5cbe68cd3ef25898c4c7c416b
